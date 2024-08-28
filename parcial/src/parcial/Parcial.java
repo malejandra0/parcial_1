44c2a667a29315027cbccc6bda222aca8e700c66
malejandra0 <malejandrarubio@ucundinamarca.edu.co>
2024-08-28 13:20:34 -0500

    Update Parcial.java

--- parcial/src/parcial/Parcial.java
+++ parcial/src/parcial/Parcial.java
@@ -14,28 +14,31 @@ public class Parcial {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        Scanner entra= new Scanner(System.in);
-        System.out.println("digite el ultimo numero de cedula de cada uno de los 15 alumnos");
-        int digitocedula[]= new int [15];
+        Scanner entra= new Scanner(System.in); 
+        System.out.println("digite el ultimo numero de cedula de cada uno de los 15 alumnos"); //Pedimos al usuario, los datos
+        int digitocedula[]= new int [15]; //este es el vector que guardará dichos datos
         
         for (int a=0; a<15 ;a++){
-            digitocedula[a]=entra.nextInt();
+            digitocedula[a]=entra.nextInt(); //Se utiliza un for para guardar los datos que el usuario ingreso+ó anteriormente
         } 
         
-        modelos entrada=new modelos(digitocedula);
+        modelos entrada=new modelos(digitocedula); // Este es el constructor en el main
         
-        entrada.modelo1();
+        // Este es el proceso para el llamado de los calculos de la clase hacia el main
+        entrada.modelo1(); 
         entrada.modelo2();
         entrada.modelo3();
         entrada.modelo4();
         entrada.modelo5();
-        
+
+        //retorno de los resultados
         int modelo1=entrada.model1();
         int modelo2=entrada.model2();
         int modelo3=entrada.model3();
         int modelo4=entrada.model4();
         int modelo5=entrada.model5();
-        
+
+        //Muestra de los resultados
         System.out.println("la cantidad de estudiantes para el modelo 1 es: " +modelo1);
         System.out.println("la cantidad de estudiantes para el modelo 2 es: " +modelo2);
         System.out.println("la cantidad de estudiantes para el modelo 3 es: " +modelo3);
